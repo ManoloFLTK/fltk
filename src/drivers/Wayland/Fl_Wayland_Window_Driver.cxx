aa1ccf393ac2ad6cc51af699fd78cb826165e64a
ManoloFLTK <41016272+ManoloFLTK@users.noreply.github.com>
2021-12-08 17:00:39 +0100

    Wayland platform: fix bug in min window size when OPTION_USE_SYSTEM_LIBDECOR is ON.

--- src/drivers/Wayland/Fl_Wayland_Window_Driver.cxx
+++ src/drivers/Wayland/Fl_Wayland_Window_Driver.cxx
@@ -1353,6 +1353,23 @@ int Fl_Wayland_Window_Driver::set_cursor(const Fl_RGB_Image *rgb, int hotx, int 
 }
 
 
+// This is only to fix a bug in libdecor where what libdecor_frame_set_min_content_size()
+// does is often destroyed by libdecor-cairo.
+static void delayed_minsize(Fl_Window *win) {
+  struct wld_window *wl_win = fl_xid(win);
+  Fl_Window_Driver *driver = Fl_Window_Driver::driver(win);
+  if (wl_win->frame) {
+    float f = Fl::screen_scale(win->screen_num());
+    libdecor_frame_set_min_content_size(wl_win->frame, driver->minw()*f, driver->minh()*f);
+  }
+  bool need_resize = false;
+  int W = win->w(), H = win->h();
+  if (W < driver->minw()) { W = driver->minw(); need_resize = true; }
+  if (H < driver->minh()) { H = driver->minh(); need_resize = true; }
+  if (need_resize) win->size(W, H);
+}
+
+
 void Fl_Wayland_Window_Driver::resize(int X, int Y, int W, int H) {
   int is_a_move = (X != x() || Y != y() || Fl_Window::is_a_rescale());
   int is_a_resize = (W != w() || H != h() || Fl_Window::is_a_rescale());
@@ -1406,6 +1423,11 @@ void Fl_Wayland_Window_Driver::resize(int X, int Y, int W, int H) {
         fl_win->configured_height = H;
         xdg_surface_set_window_geometry(fl_win->xdg_surface, 0, 0, W * f, H * f);
       }
+#if defined(USE_SYSTEM_LIBDECOR) && USE_SYSTEM_LIBDECOR
+      if (W < minw() || H < minh()) {
+        Fl::add_timeout(0.01, (Fl_Timeout_Handler)delayed_minsize, pWindow);
+      }
+#endif
     } else {
      if (!in_handle_configure && fl_win->xdg_toplevel) {
       // Wayland doesn't seem to provide a reliable way for the app to set the window position on screen.
