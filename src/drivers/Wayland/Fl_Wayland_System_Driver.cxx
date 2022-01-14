08053d276d2e0777a49a852eb3c7f1963443961d
ManoloFLTK <41016272+ManoloFLTK@users.noreply.github.com>
2022-01-14 19:38:57 +0100

    Wayland platform: remove member function make_transient from class Fl_Wayland_System_Driver

--- src/drivers/Wayland/Fl_Wayland_System_Driver.cxx
+++ src/drivers/Wayland/Fl_Wayland_System_Driver.cxx
@@ -16,23 +16,7 @@
 //
 
 #include "Fl_Wayland_System_Driver.H"
-#include <FL/Fl_File_Browser.H>
-#include <FL/fl_string.h>  // fl_strdup
-#include <FL/platform.H>
-#include "../../flstring.h"
-#include "Fl_Wayland_Screen_Driver.H"
-#include "Fl_Wayland_Window_Driver.H"
-#include "xdg-shell-client-protocol.h"
-
-#include <locale.h>
-#include <time.h>
-#include <unistd.h>
-#include <sys/types.h>
-#include <pwd.h>
-#include <string.h>     // strerror(errno)
-#include <errno.h>      // errno
-#include <dlfcn.h>   // for dlsym
-
+#include <FL/Fl.H>
 
 /**
  Creates a driver that manages all system related calls.
@@ -42,38 +26,6 @@
 Fl_System_Driver *Fl_System_Driver::newSystemDriver()
 {
   return new Fl_Wayland_System_Driver();
-}
-
-
-void Fl_Wayland_System_Driver::make_transient(void *ptr_gtk, void *gtk_window, Fl_Window *win) {
-  //TODO
-  typedef struct _GdkDrawable GdkWindow;
-  typedef struct _GtkWidget GtkWidget;
-
-  typedef GdkWindow* (*XX_gtk_widget_get_window_type)(GtkWidget *);
-  static XX_gtk_widget_get_window_type fl_gtk_widget_get_window = NULL;
-
-  typedef struct wl_surface *(*XX_gdk_wayland_window_get_wl_surface_type)(GdkWindow *);
-  static XX_gdk_wayland_window_get_wl_surface_type fl_gdk_wayland_window_get_wl_surface = NULL;
-
-  if (!fl_gtk_widget_get_window) {
-    fl_gtk_widget_get_window = (XX_gtk_widget_get_window_type)dlsym(ptr_gtk, "gtk_widget_get_window");
-    if (!fl_gtk_widget_get_window) return;
-  }
-
-  GdkWindow* gdkw = fl_gtk_widget_get_window((GtkWidget*)gtk_window);
-
-  if (!fl_gdk_wayland_window_get_wl_surface) {
-    fl_gdk_wayland_window_get_wl_surface = (XX_gdk_wayland_window_get_wl_surface_type)dlsym(ptr_gtk, "gdk_wayland_window_get_wl_surface");
-  }
-  struct wl_surface *wld_surf = fl_gdk_wayland_window_get_wl_surface(gdkw);
-  // but what next?
-/* not good:
-Fl_Wayland_Screen_Driver *scr_driver = (Fl_Wayland_Screen_Driver*)Fl::screen_driver();
-struct xdg_surface *xdgs = xdg_wm_base_get_xdg_surface(scr_driver->xdg_wm_base, wld_surf);
-struct xdg_toplevel *top = xdg_surface_get_toplevel(xdgs);
-xdg_toplevel_set_parent(top, fl_xid(win)->xdg_toplevel);
-*/
 }
 
 
