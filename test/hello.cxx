9b820ce77b1556d9b093bfb343bdbf963257ba16
ManoloFLTK <41016272+ManoloFLTK@users.noreply.github.com>
2024-09-16 00:07:57 +0200

    Add Fl_Native_Text_Widget::kind(enum kind)

--- test/hello.cxx
+++ test/hello.cxx
@@ -7,10 +7,10 @@ int main(int argc, char **argv) {
   bool readonly = false;
   bool selectable = true;
   const char *label;
-  if (readonly && !selectable) label = "Read only text box";
-  else if (readonly && selectable) label = "Selectable text box";
-  else label = "Editable text box";
-  Fl_Native_Text_Widget *box = new Fl_Native_Text_Widget(20, 40, 400, 200, label);
+  if (readonly && !selectable) label = "Read only text boxes";
+  else if (readonly && selectable) label = "Selectable text boxes";
+  else label = "Editable text boxes";
+  Fl_Native_Text_Widget *box = new Fl_Native_Text_Widget(20, 40, 400, 100, label);
   box->align(FL_ALIGN_TOP);
   box->textfont(FL_COURIER_BOLD);
   box->textsize(20);
@@ -20,13 +20,26 @@ int main(int argc, char **argv) {
   box->box(FL_DOWN_FRAME);
   box->right_to_left(true);
   box->value(
-    "بداية القرن التاسع عشر أول مشروع لرسم خرائط الموقع، تبعه العديد من مواطنيه لا سيما«أوستن هنري لايارد» في عام 1850 و«هنري روليِنسون»"
-             );
-  //box->wrap(0);
+//             "بداالتاسع عشر أول من مواطنيه"
+            "بداية القرن التاسع عشر أول مشروع لرسم خرائط الموقع، تبعه العديد من مواطنيه لا سيما«أوستن هنري لايارد» في عام 1850 وهنري روليِنسون"
+    //"Request a notification when it is a good time to start drawing a new frame, by creating a frame callback."
+            );
+  Fl_Native_Text_Widget *box2 = new Fl_Native_Text_Widget(20, box->y()+box->h()+10, 400, 50, label);
+  box2->kind(Fl_Native_Text_Widget::SINGLE_LINE);
+  box2->textfont(box->textfont());
+  box2->textsize(box->textsize());
+  box2->textcolor(box->textcolor());
+  box2->cursor_color(FL_RED);
+  box2->color(box->color());
+  box2->box(box->box());
+  box2->right_to_left(box->right_to_left());
+  box2->value("بداالتاسع عشر أول من مواطنيه");
   window->end();
   window->resizable(box);
   box->readonly(readonly);
   box->selectable(selectable);
   window->show();
-  return Fl::run();
+  Fl::run();
+  delete window;
+  return 0;
 }
