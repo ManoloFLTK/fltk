d403238ad5bc5b854497f3bf8f421710252f2061
ManoloFLTK <41016272+ManoloFLTK@users.noreply.github.com>
2025-03-29 08:34:28 +0100

    Move platform-specific code to adequate place in source tree

--- src/Fl_Dockable_Window.cxx
+++ src/Fl_Dockable_Window.cxx
@@ -17,15 +17,25 @@
  Fl_Dockable_Window implementation.
  */
 
-
-#include <FL/platform.H>
 #include <FL/Fl_Dockable_Window.H>
+#include "Fl_Dockable_Window_Driver.H"
+
+
+Fl_Dockable_Window::Fl_Dockable_Window(int x, int y, int w, int h, const char *t) : Fl_Double_Window(x,y,w,h,t) {
+  target_box_ = NULL;
 #ifdef FLTK_USE_WAYLAND
-#  include "../src/drivers/Wayland/Fl_Wayland_Screen_Driver.H"
-#  include "../src/drivers/Wayland/Fl_Wayland_Window_Driver.H"
-#  include "../src/xdg-shell-client-protocol.h"
-#  include "../src/xdg-toplevel-drag-client-protocol.h"
-#endif // FLTK_USE_WAYLAND
+  if (fl_wl_display()) {
+    driver_ = new Fl_Wayland_Dockable_Window_Driver();
+    return;
+  }
+#endif
+  driver_ = new Fl_Dockable_Window_Driver();
+}
+
+
+void Fl_Dockable_Window_Driver::delete_win_cb(Fl_Dockable_Window *w, void *) {
+  w->driver_->delete_win(w);
+}
 
 
 void Fl_Dockable_Window::target_box(Fl_Boxtype bt, int x, int y, int w, int h, const char *t, Fl_Group *g) {
@@ -33,214 +43,87 @@ void Fl_Dockable_Window::target_box(Fl_Boxtype bt, int x, int y, int w, int h, c
   else {
     Fl_Group *save = Fl_Group::current();
     Fl_Group::current(NULL);
-#ifdef FLTK_USE_WAYLAND
-    if (fl_wl_display()) target_box_ = new target_box_class(bt, x, y, w, h, t);
-    else
-#endif
-    target_box_ = new Fl_Box(bt, x, y, w, h, t);
+    target_box_ = driver_->new_target_box(bt, x, y, w, h, t);
     g->add(target_box_);
     Fl_Group::current(save);
   }
 }
 
 
-void Fl_Dockable_Window::delete_win(Fl_Dockable_Window *dock, void *) {
-#ifdef FLTK_USE_WAYLAND
-  delete dock->old_dock_;
-  if (dock->drag_) xdg_toplevel_drag_v1_destroy(dock->drag_);
-#endif
+void Fl_Dockable_Window_Driver::delete_win(Fl_Dockable_Window *dock) {
   delete dock;
 }
 
 
-int Fl_Dockable_Window::drag_box_out::handle(int event) {
+int Fl_Dockable_Window_Driver::drag_box_out::handle(int event) {
+  Fl_Dockable_Window *dock = (Fl_Dockable_Window*)window();
+  return Fl_Dockable_Window_Driver::driver(dock)->handle(this, event);
+}
+
+
+int Fl_Dockable_Window_Driver::handle(Fl_Dockable_Window_Driver::drag_box_out *box, int event) {
   static int fromx, fromy, winx, winy;
-  if (event == FL_PUSH && (Fl::event_state() & FL_BUTTON1) && window()->parent()) {
-    Fl_Dockable_Window *dock = (Fl_Dockable_Window*)window();
-    Fl_Window *top = top_window();
-#ifdef FLTK_USE_WAYLAND
-    if (fl_wl_display()) {
-      struct wld_window *xid = fl_wl_xid(dock);
-      Fl_Wayland_Screen_Driver *scr_driver = (Fl_Wayland_Screen_Driver*)Fl::screen_driver();
-      xdg_toplevel_set_parent(xid->xdg_toplevel, Fl_Wayland_Window_Driver::driver(top)->xdg_toplevel());
-      scr_driver->seat->data_source = wl_data_device_manager_create_data_source(scr_driver->seat->data_device_manager);
-      wl_data_source_add_listener(scr_driver->seat->data_source, Fl_Wayland_Screen_Driver::p_data_source_listener, (void*)0);
-      wl_data_source_offer(scr_driver->seat->data_source, "xdg_toplevel_drag_manager");
-      wl_data_source_set_actions(scr_driver->seat->data_source, WL_DATA_DEVICE_MANAGER_DND_ACTION_COPY);
-      dock->drag_ =
-        xdg_toplevel_drag_manager_v1_get_xdg_toplevel_drag(scr_driver->xdg_toplevel_drag, scr_driver->seat->data_source);
-      //printf("start_drag surface=%p serial=%u\n",xid->wl_surface, scr_driver->seat->serial);
-      wl_data_device_start_drag(scr_driver->seat->data_device, scr_driver->seat->data_source, xid->wl_surface, NULL,
-                                scr_driver->seat->serial);
-      Fl_Wayland_Window_Driver::driver(dock)->unmap();
-      top->remove(dock);
-      Fl_Dockable_Window *old = dock;
-      dock = dock->copy("dragged");
-    } else
-#endif
-    {
+  Fl_Dockable_Window *dock = (Fl_Dockable_Window*)box->window();
+  if (event == FL_PUSH && (Fl::event_state() & FL_BUTTON1) && dock->parent()) {
+    Fl_Window *top = dock->top_window();
+    
       dock->hide();
       top->remove(dock);
       dock->position(top->x() + dock->x() , top->y() + dock->y() );
-    }
-    dock->callback((Fl_Callback*)delete_win);
-    label("Drag");
+    
+    dock->callback((Fl_Callback*)Fl_Dockable_Window_Driver::delete_win_cb);
+    box->label("Drag");
     dock->border(0);
-    can_dock_ = false;
+    box->can_dock_ = false;
     dock->show();
-#ifdef FLTK_USE_WAYLAND
-    if (fl_wl_display()) {
-      struct wld_window *xid = fl_wl_xid(dock);
-      //xdg_toplevel_set_parent(xid->xdg_toplevel, Fl_Wayland_Window_Driver::driver(top)->xdg_toplevel());
-      xdg_toplevel_drag_v1_attach(dock->drag_, xid->xdg_toplevel, Fl::event_x() - x(), Fl::event_y() - y());
-      //printf("xdg_toplevel_drag_v1_attach to toplevel=%p\n",xid->xdg_toplevel);
-    }
-#endif
     return 1;
-#ifdef FLTK_USE_WAYLAND
-  } else if (event == FL_PUSH && fl_wl_display() && (Fl::event_state() & FL_BUTTON1)) {
-    // catch again a draggable window
-    Fl_Wayland_Screen_Driver *scr_driver = (Fl_Wayland_Screen_Driver*)Fl::screen_driver();
-    Fl_Dockable_Window *dock = (Fl_Dockable_Window*)window();
-    if (dock->drag_) xdg_toplevel_drag_v1_destroy(dock->drag_);
-    scr_driver->seat->data_source = wl_data_device_manager_create_data_source(scr_driver->seat->data_device_manager);
-    wl_data_source_add_listener(scr_driver->seat->data_source, Fl_Wayland_Screen_Driver::p_data_source_listener, (void*)0);
-    wl_data_source_offer(scr_driver->seat->data_source, "xdg_toplevel_drag_manager");
-    wl_data_source_set_actions(scr_driver->seat->data_source, WL_DATA_DEVICE_MANAGER_DND_ACTION_COPY);
-    dock->drag_ = xdg_toplevel_drag_manager_v1_get_xdg_toplevel_drag(scr_driver->xdg_toplevel_drag, scr_driver->seat->data_source);
-    struct wld_window *xid = fl_wl_xid(dock);
-    //printf("start_drag surface=%p serial=%u\n",scr_driver->seat->pointer_focus, scr_driver->seat->serial);
-    wl_data_device_start_drag(scr_driver->seat->data_device, scr_driver->seat->data_source,
-                              scr_driver->seat->pointer_focus, NULL, scr_driver->seat->serial);
-    //printf("xdg_toplevel_drag_v1_attach to toplevel=%p\n",xid->xdg_toplevel);
-    // need to attach AFTER start_drag even though xdg_toplevel_drag protocol doc says opposite!
-    xdg_toplevel_drag_v1_attach(dock->drag_, xid->xdg_toplevel, Fl::event_x() - x(), Fl::event_y() - y());
-    return 1;
-#endif
   } else if (event == FL_PUSH && (Fl::event_state() & FL_BUTTON1)) {
     fromx = Fl::event_x_root();
     fromy = Fl::event_y_root();
-    winx = window()->x_root();
-    winy = window()->y_root();
+    winx = dock->x_root();
+    winy = dock->y_root();
     return 1;
-  } else if (event == FL_DRAG && !window()->parent()
-#ifdef FLTK_USE_WAYLAND
-             && !fl_wl_display()
-#endif
-             ) {
+  } else if (event == FL_DRAG && !dock->parent()) {
     int deltax = Fl::event_x_root() - fromx;
     int deltay = Fl::event_y_root() - fromy;
-    window()->position(winx + deltax, winy + deltay);
-    Fl_Dockable_Window *dock = (Fl_Dockable_Window*)window();
+    dock->position(winx + deltax, winy + deltay);
     Fl_Box *target = dock->target_box();
     if (target) {
       int target_x = target->window()->x() + target->x();
       int target_y = target->window()->y() + target->y();
-      can_dock_ = (window()->x() + window()->w() >= target_x &&
-                   window()->x() < target_x + target->w() &&
-                   window()->y() >= target_y && window()->y() < target_y + target->h());
-      if (can_dock_){
-        label("Dock");
+      box->can_dock_ = (dock->x() + dock->w() >= target_x &&
+                   dock->x() < target_x + target->w() &&
+                   dock->y() >= target_y && dock->y() < target_y + target->h());
+      if (box->can_dock_){
+        box->label("Dock");
         target->color(FL_RED);
         target->redraw();
       } else {
-        label("Drag");
+        box->label("Drag");
         target->color(FL_BACKGROUND_COLOR);
         target->redraw();
       }
     }
     return 1;
-  } else if (event == FL_RELEASE && !window()->parent() && can_dock_
-#ifdef FLTK_USE_WAYLAND
-             && !fl_wl_display()
-#endif
-             ) {
-    Fl_Dockable_Window *dock = (Fl_Dockable_Window*)window();
+  } else if (event == FL_RELEASE && !dock->parent() && box->can_dock_) {
     Fl_Box *target = dock->target_box();
     Fl_Window *parent = target->window();
     target->hide();
-    Fl_Window *win = window();
-    win->hide();
-    parent->add(win);
-    win->position(target->x(), target->y());
-    label("Undock");
+    dock->hide();
+    parent->add(dock);
+    dock->position(target->x(), target->y());
+    box->label("Undock");
     dock->target_box(FL_NO_BOX, 0, 0, 0, 0, NULL, NULL);
     delete target;
-    win->show();
+    dock->show();
     return 1;
   }
-  return Fl_Box::handle(event);
+  return box->Fl_Box::handle(event);
 }
 
 
 void Fl_Dockable_Window::command_box(int x, int y, int w, int h, const char *t) {
-  drag_box_out *drag = new drag_box_out(FL_DOWN_BOX, x,y,w,h, t);
+  Fl_Dockable_Window_Driver::drag_box_out *drag = new Fl_Dockable_Window_Driver::drag_box_out(FL_DOWN_BOX, x,y,w,h, t);
   this->add(drag);
-#ifdef FLTK_USE_WAYLAND
-  command_box_ = drag;
-#endif
+  driver_->command_box(drag);
 }
-
-
-#ifdef FLTK_USE_WAYLAND
-
-Fl_Dockable_Window *Fl_Dockable_Window::copy(const char *t) {
-  Fl_Dockable_Window *dock2 = new Fl_Dockable_Window(x(), y(), w(), h(), t);
-  while (children()) {
-    Fl_Widget *wid = child(0);
-    remove(wid);
-    dock2->add(wid);
-  }
-  target_box()->user_data(dock2);
-  dock2->resizable( resizable() == this ? dock2 : resizable() );
-  dock2->target_box_ = target_box_;
-  dock2->drag_ = drag_;
-  dock2->command_box_ = command_box_;
-  dock2->old_dock_ = this;
-  dock2->user_data( user_data() );
-  user_data(NULL);
-  dock2->color( color() );
-  dock2->box( box() );
-  return dock2;
-}
-
-
-int Fl_Dockable_Window::target_box_class::handle(int event) {
-  Fl_Dockable_Window *dock = (Fl_Dockable_Window*)user_data();
-  if (event == FL_DND_ENTER) {
-    //puts("FL_DND_ENTER");
-    dock->command_box_->label("Dock"); dock->command_box_->redraw_label();
-    color(FL_RED); redraw();
-    return 1;
-  } else if (event == FL_DND_DRAG) {
-    //puts("FL_DND_DRAG");
-    return 1;
-  } else if (event == FL_DND_LEAVE) {
-    //puts("FL_DND_LEAVE");
-    dock->command_box_->label("Drag"); dock->command_box_->redraw_label();
-    color(FL_BACKGROUND_COLOR); redraw();
-    return 1;
-  } else if (event == FL_DND_RELEASE) {
-    //puts("FL_DND_RELEASE");
-    xdg_toplevel_drag_v1_destroy(dock->drag_);
-    Fl_Box *target = this;
-    Fl_Window *parent = target->window();
-    target->hide();
-    user_data(NULL);
-    dock->hide();
-    parent->add(dock);
-    dock->position(target->x(), target->y());
-    label("Undock");
-    delete target;
-    dock->show();
-    dock->command_box_->label("Docked");
-    dock->command_box_->color(FL_BACKGROUND_COLOR);
-    delete dock->old_dock_;
-    dock->old_dock_ = NULL;
-    return 0; // not to generate FL_PASTE event
-  }
-  return Fl_Box::handle(event);
-}
-
-#endif // FLTK_USE_WAYLAND
-
