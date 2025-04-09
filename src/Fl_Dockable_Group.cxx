8b19d3e60fad0d92ebe7d617f97ed24d99fa1bfb
ManoloFLTK <41016272+ManoloFLTK@users.noreply.github.com>
2025-04-09 15:23:07 +0200

    Add return value to member Fl_Dockable_Group::command_box(int,int,int,int)

--- src/Fl_Dockable_Group.cxx
+++ src/Fl_Dockable_Group.cxx
@@ -83,13 +83,15 @@ int Fl_Dockable_Group::target_index() { return Fl_Dockable_Group_Driver::target_
  and dragging the object. The location and size of the 'command box' inside the Fl_Dockable_Group
  can be freely set by the caller. Its changing colors and labels are best set via member functions color_for_states()
  and label_for_states(). Its boxtype is FL_DOWN_BOX by default but can be be freely changed.
+ \return The created 'command box'.
  */
-void Fl_Dockable_Group::command_box(int x, int y, int w, int h) {
+Fl_Box *Fl_Dockable_Group::command_box(int x, int y, int w, int h) {
   Fl_Dockable_Group_Driver::cmd_box_class *drag =
     new Fl_Dockable_Group_Driver::cmd_box_class(FL_DOWN_BOX, x, y, w, h, NULL);
   this->add(drag);
   command_box_ = drag;
   driver_->state(UNDOCK);
+  return drag;
 }
 
 
